79568426e3cf973e7e3b6aef8f5ef2a3fa2d4101
Faizan-zia340 <154322813+Faizan-zia340@users.noreply.github.com>
2024-01-28 13:44:00 +0500

    factorial,multiply number ,and divide two numbers

---
+++ divide two numbers .py
@@ -0,0 +1,8 @@
+def divide(x,y):
+    result=x/y
+    return result
+
+num1 = 30
+num2 = 3
+result = divide(num1, num2)
+print(f"The result of dividing {num1} and {num2} is: {result}")
---
+++ multiply two numbers.py
@@ -0,0 +1,9 @@
+def multiply(x, y):
+    result = x * y
+    return result
+
+# Example usage:
+num1 = 5
+num2 = 7
+result = multiply(num1, num2)
+print(f"The result of multiplying {num1} and {num2} is: {result}")
